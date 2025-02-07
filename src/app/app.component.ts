2ed20abd909291a24d25ff09a602cf61b28b69b0
JosephFrost <josephnina16forjob@gmail.com>
2025-02-06 21:54:49 -0400

    First project

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Inicio', url: '/inicio', icon: 'home' },
+    { title: 'Sumadora', url: '/sumadora', icon: 'calculator'},
+    { title: 'Traductor', url: '/traductor', icon: 'language'},
+    { title: 'Tabla de Multiplicar', url: '/multiplicar', icon: 'grid' },
+    { title: 'Experiencia Personal', url: '/experiencia', icon: 'person' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  constructor() {}
+}
